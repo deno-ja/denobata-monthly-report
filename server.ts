54fb53505751ebd5752b21191a67a642cb5863da
WhyK <windchimeyk@gmail.com>
2021-09-20 03:28:23 +0900

    first commit

---
+++ server.ts
@@ -0,0 +1,18 @@
+import { Diplodocus } from "https://deno.land/x/diplodocus@0.0.1/mod.ts";
+
+const diplodocus = new Diplodocus();
+
+const PORT = 8080;
+const listener = Deno.listen({ port: PORT });
+console.log(`HTTP server listening on http://localhost:${PORT}`);
+
+async function handleConn(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const e of httpConn) {
+    e.respondWith(diplodocus.handler(e.request));
+  }
+}
+
+for await (const conn of listener) {
+  handleConn(conn);
+}
